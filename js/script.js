f70398860a9481bece3705338fd72a50b51218da
lulumoha <lulumoha123@gmail.com>
2017-11-11 14:24:24 +0300

    subsequent commits

---
+++ js/script.js
@@ -0,0 +1,38 @@
+
+function counter (userInput) {
+  var userInputNum = parseInt(userInput);
+  var userArray = [];
+  for (var i = 1; i <= userInputNum; i++) {
+    if (i % 3 == 0 && i % 5 ==0) {
+      userArray.push('Ping-Pong');
+    } else if (i % 3 == 0) {
+      userArray.push('Ping');
+    } else if (i % 5 == 0) {
+      userArray.push('Pong');
+    } else {
+      userArray.push(i);
+    }
+  };
+  return userArray;
+};
+
+function errorCheck (errorInput) {
+  var checkInput = parseInt(errorInput);
+  if (!checkInput || checkInput < 0 || isNaN(checkInput)){
+    return true;
+  }
+};
+
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    $("#list").empty();
+    event.preventDefault();
+    if (errorCheck($("#number-input").val())) {
+        alert("Enter a number! No negatives please!");
+    } else
+    var pingPong = counter($("#number-input").val());
+    pingPong.forEach(function (value) {
+      $("#list").append("<li>" + value + "</li>");
+    });
+  });
+});
